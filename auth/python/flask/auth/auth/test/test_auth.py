659e7fc15e45e85d378751d9834fe668e8432860
Sumanth Vepa <svepa@milestone42.com>
2022-05-13 09:41:06 +0530

    [issue002a1] Modify auth_test to test if http call to / works
    FEATURE(s):
    issue002a-auth-python:
      Create the auth application using python
      issue002a1-auth-python-flask:
        Create the auth application using python and flask
    
    FILE(s):
    [modified]auth/python/flask/auth/auth/test/test_auth.py
    
    DESCRIPTION
    Modify auth_test to test if http call to / works

--- auth/python/flask/auth/auth/test/test_auth.py
+++ auth/python/flask/auth/auth/test/test_auth.py
@@ -1,10 +1,14 @@
 import unittest
 
-import auth
+from auth import app
 
 class TestHomePage(unittest.TestCase):
   def test_non_loggedin_home_page(self):
-    self.fail('Not implemented')
+    tester = app.test_client(self)
+    response = tester.get('/', content_type='text/html')
+    self.assertEqual(response.status_code, 200);
+    # TODO: Test that all the links in the returned page
+    # also work
 
 if __name__ == '__main__':
   unittest.main();
