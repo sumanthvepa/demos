c4b3248c84aa9f3dea15ee62471bdadeab8112bf
Sumanth Vepa <svepa@milestone42.com>
2022-05-13 08:47:48 +0530

    [issue002a1] Add a skeleton test suite (does nothing expect fail)
    FEATURE(s):
    issue002a-auth-python:
      Create the auth application using python
      issue002a1-auth-python-flask:
        Create the auth application using python and flask
    
    FILE(s):
    [new]auth/python/flask/auth/auth/test/.gitignore
    [new]auth/python/flask/auth/auth/test/__init__.py
    [new]auth/python/flask/auth/auth/test/run_tests.sh
    [new]auth/python/flask/auth/auth/test/test_auth.py
    
    DESCRIPTION
    Add a skeleton test suite (does nothing expect fail)

---
+++ auth/python/flask/auth/auth/test/run_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PYTHONPATH=.. python3 test_auth.py
---
+++ auth/python/flask/auth/auth/test/test_auth.py
@@ -0,0 +1,10 @@
+import unittest
+
+import auth
+
+class TestHomePage(unittest.TestCase):
+  def test_non_loggedin_home_page(self):
+    self.fail('Not implemented')
+
+if __name__ == '__main__':
+  unittest.main();
