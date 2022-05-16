1cc835796f0e69bf1ba4e956e43140fb884a83c2
Sumanth Vepa <svepa@milestone42.com>
2022-05-16 10:37:19 +0530

    [issue002a1] Implement full authentication and signin errors w/ dummy users
    FEATURE(s):
    issue002a-auth-python:
      Create the auth application using python
      issue002a1-auth-python-flask:
        Create the auth application using python and flask
    
    FILE(s):
    [modified]auth/python/flask/auth/auth/auth.py
    [modified]auth/python/flask/auth/auth/templates/signin.jinja2
    
    DESCRIPTION
    Implement full authentication and signin errors w/ dummy users
    
    Modify the authenticate_user_credentials function to do proper
    authentication but against a dummy set of users stored as a constant
    dictionary in the code.
    
    Report errors in authentication correctly in the signin page.
    
    In the event of an error, repopulate fields in the signin page with
    their previously entered values.

--- auth/python/flask/auth/auth/auth.py
+++ auth/python/flask/auth/auth/auth.py
@@ -1,5 +1,7 @@
 import os
 import secrets
+
+import bcrypt
 from flask import (
   Flask,
   redirect,
@@ -26,13 +28,34 @@ def is_valid_redirect_url(redirect_url):
   VALID_REDIRECTS = [url_for('home_page')]
   return redirect_url in VALID_REDIRECTS
 
+class SignInError(RuntimeError):
+  pass
+
+# TODO: Move these dictionaries to authws and fetch them from the database
+valid_usernames = {'luser': {'display_name': 'Lawrence Ugnon Ser'} }
+valid_emails = { 'luser@example.com': {'display_name': 'Lawrence Ugnon Ser'} }
+valid_passwords = {
+  'luser': bcrypt.hashpw('12345'.encode('utf-8'), bcrypt.gensalt(16))
+}
 
 def authenticate_credentials(username_email, password):
-  # TODO: Implement this.
-  return {
-    'username': 'sumanthvepa',
-    'display_name': 'Sumanth Vepa',
-    'email': 'svepa@milestone42.com' }
+  # TODO: Move this all this code to authws
+  if username_email in valid_usernames:
+    user = valid_usernames[username_email]
+  elif username_email in valid_emails:
+    user = valid_emails[username_email]
+  else:
+    raise SignInError(
+      'We could not find this username or email in our records')
+
+  password = password.encode('utf-8')
+  if not bcrypt.checkpw(password, valid_passwords[username_email]):
+    raise SignInError(
+      'The password you entered did not '
+      + 'match with the one we have on file for you.')
+
+  return user
+
 
 @app.route('/', methods=['GET'])
 def home_page():
@@ -54,11 +77,19 @@ def home_page():
 
 @app.route('/signin/', methods=['GET'])
 def signin_page():
-  # Get any error object in the session
-  # This object is set if there were errors in a previous
+  # Get the error object and related keys
+  # These objects are set if there were errors in a previous
   # attempt at sign in, and the user is reaching this
   # URL via redirect back from a failed form processing.
   error = session.get('error', None)
+  if error:
+    del session['error']
+  default_username_email = session.get('default_username_email', None)
+  if default_username_email:
+    del session['default_username_email']
+  default_password = session.get('default_password', None)
+  if default_password:
+    del session['default_password']
 
   # Get the redirect URL (This will be set if 
   # if the Sign In is triggered by a redirect from
@@ -78,20 +109,31 @@ def signin_page():
   return render_template(
     'signin.jinja2',
     error=error,
+    default_username_email=default_username_email,
+    default_password=default_password,
     redirect_url=redirect_url,
     csrf_token=csrf_token)
 
 @app.route('/signin/', methods=['POST'])
 def process_signin():
+  # Reset the session user field.
+  if 'user' in session:
+    del session['user']
+
+  # Clean up the error related values in the session
+  if 'error' in session:
+    del session['error']
+  if default_username_email:
+    del session['default_username_email']
+  if default_password:
+    del session['default_password']
+
   # Check CSRF token and return 401 if the CSRF token
   # does not match the one stored in the session.
   form_csrf_token = request.form.get('csrf_token', None)
   session_csrf_token = session.get('csrf_token', None)
   if form_csrf_token != session_csrf_token:
     abort(401)
-
-  username_email = request.form.get('username_email', None)
-  password = request.form.get('password', None)
 
   # Get the redirect URL from the from the hidden input
   # field in the form. Use that (after validation) to
@@ -101,18 +143,21 @@ def process_signin():
   if not is_valid_redirect_url(redirect_url):
     redirect_url = url_for('home_page')
 
-  # Reset the session error field.
-  if 'error' in session:
-    del session['error']
+  # Get the username and email from the form
+  username_email = request.form.get('username_email', None)
+  password = request.form.get('password', None)
 
-  # Reset the session user field.
-  if 'user' in session:
-    del session['user']
-
-  user = authenticate_credentials(username_email, password)
-  session['user'] = user
-
-  return redirect(redirect_url)
+  # Get the user by authenticating their credentials or
+  # fail and take the visitor back to the signin page with errors
+  try:
+    user = authenticate_credentials(username_email, password)
+    session['user'] = user
+    return redirect(redirect_url)
+  except SignInError as ex:
+    session['error'] = str(ex)
+    session['default_username_email'] = username_email
+    session['default_password'] = password
+    return redirect(url_for('signin_page') + '?redirect_url='+redirect_url)
   
 
 @app.route('/signout/', methods=['POST'])
