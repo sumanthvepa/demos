c7bfad1c9324a9b194fe0cef95de5b234af12e84
Sumanth Vepa <svepa@milestone42.com>
2022-05-20 14:55:14 +0530

    [issue002a1] Release the code under GPL v3
    FEATURE(s):
    issue002a-auth-python:
      Create the auth application using python
      issue002a1-auth-python-flask:
        Create the auth application using python and flask
    
    FILE(s):
    [new]COPYING
    [modified]auth/db/erase_db.sql
    [modified]auth/db/erase_secrets_db.sql
    [modified]auth/db/get_secret_key.sh
    [modified]auth/db/initialize_db.sql
    [modified]auth/db/initialize_secrets_db.sql
    [modified]auth/db/insert_secret_key.sql
    [modified]auth/python/flask/auth/auth/auth.py
    [modified]auth/python/flask/auth/auth/static/auth.css
    [modified]auth/python/flask/auth/auth/templates/index.jinja2
    [modified]auth/python/flask/auth/auth/templates/signin.jinja2
    [modified]auth/python/flask/auth/authws/authws.py
    [modified]auth/python/flask/auth/run-app.sh
    [modified]auth/python/flask/auth/run-authws.sh
    [modified]auth/python/flask/auth/tools/generate_secret_key

--- auth/db/get_secret_key.sh
+++ auth/db/get_secret_key.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+#####################################################################
+# This file is part of demos.
+# Demos is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# Demos is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+######################################################################
 
 echo `mysql --user=svepa --password secrets -N -s -e 'select secret_value from secret where environment_variable_name="AUTH_APP_FLASK_SECRET_KEY" and environment_name="development"'`
