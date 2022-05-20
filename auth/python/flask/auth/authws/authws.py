2ea1a195e7abde92ee3df50ea795a5d96ac225c3
Sumanth Vepa <svepa@milestone42.com>
2022-05-20 11:05:53 +0530

    [issue002a1] authws.py: Add doc comments
    FEATURE(s):
    issue002a-auth-python:
      Create the auth application using python
      issue002a1-auth-python-flask:
        Create the auth application using python and flask
    
    FILE(s):
    [modified]auth/python/flask/auth/authws/authws.py
      Add doc comments

--- auth/python/flask/auth/authws/authws.py
+++ auth/python/flask/auth/authws/authws.py
@@ -8,6 +8,7 @@ from flask import Flask, request
 
 
 def create_app():
+  """ Create a properly configured auth web service app"""
   authws_app = Flask(__name__)
   authws_app.secret_key = os.environ.get('AUTHWS_FLASK_SECRET_KEY')
   if authws_app.secret_key is None:
@@ -20,13 +21,26 @@ app = create_app()
 
 
 class AuthStatus(Enum):
+  """ A Smart Enum that that describes all the errors that can possibly
+      occur in the operation of the auth web service """
+
   def __new__(cls, *args, **kwds):
+    """
+      This method assigns every member of this enum a distinct
+      ordinal count that enables each enum member to have distinct
+      value. (This is different from the error_code which need not
+      be sequentially assigned)
+    """
     value = len(cls.__members__) + 1
     obj = object.__new__(cls)
     obj._value = value
     return obj
 
   def __init__(self, http_status, description):
+    """
+      Initializes an enum instance with an error_code, http status and error
+      description
+    """
     super().__init__()
     self.error_code = self._value - 1
     self.http_status = http_status
@@ -62,6 +76,8 @@ class AuthStatus(Enum):
 
 
 class AuthError(RuntimeError):
+  """ The exception raised when the auth web service encounters a problem"""
+
   def __init__(self, status):
     super().__init__(status.description)
     self.status = status
@@ -72,11 +88,13 @@ class AuthError(RuntimeError):
 
 @app.errorhandler(AuthError)
 def handle_authentication_errors(ex):
+  """ Convert AuthError into a JSON error response """
   return ex.to_tuple()
 
 
 @app.errorhandler(HTTPException)
 def handle_http_exceptions(ex):
+  """ Convert HTTPException into a JSON error response"""
   auth_status = AuthStatus.OTHER_ERROR
   if 400 <= ex.code < 500:
     auth_status = AuthStatus.CLIENT_ERROR
@@ -87,6 +105,11 @@ def handle_http_exceptions(ex):
 
 @app.errorhandler(Exception)
 def handle_errors(ex):
+  """
+    Convert Exception into a JSON error response
+
+    This is a catchall error handler.
+  """
   return AuthStatus.INTERNAL_ERROR.to_tuple(str(ex))
 
 
@@ -109,14 +132,17 @@ by_email = dict(zip([user['email'] for user in users.values()], users.values()))
 
 
 def find_user_by_username(username):
+  # TODO: Replace this with real code
   return by_username.get(username, None)
 
 
 def find_user_by_email(email):
+  # TODO: Replace this with real code.
   return by_email.get(email, None)
 
 
 def find_user_by_username_or_email(username_email):
+  # TODO: Replace this with real code.
   from_username = find_user_by_username(username_email)
   from_email = find_user_by_email(username_email)
   user = from_username or from_email
@@ -126,6 +152,7 @@ def find_user_by_username_or_email(username_email):
 
 
 def user_from_user_id(user_id):
+  # TODO: Replace this with real code.
   user = users.get(str(user_id), None)
   if not user:
     raise AuthError(AuthStatus.NO_SUCH_USER)
@@ -134,6 +161,7 @@ def user_from_user_id(user_id):
 
 @app.route('/api/users/', methods=['GET'])
 def find_user():
+  """ Return user id given a username or email """
   username_email = request.args.get('username_email')
   if not username_email:
     raise AuthError(AuthStatus.NO_USERNAME_EMAIL_SUPPLIED)
@@ -142,6 +170,13 @@ def find_user():
 
 @app.route('/api/user/<int:user_id>/authenticate', methods=['GET'])
 def authenticate_user(user_id):
+  """
+    Authenticate a user given a user id and password.
+
+    Returns a JSON object with a token that can be used for further
+    queries.
+
+  """
   user = user_from_user_id(user_id)
   password = request.args.get('password')
   if not password:
@@ -157,6 +192,12 @@ def authenticate_user(user_id):
 
 @app.route('/api/user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
+  """
+    Return a JSON user object for a given user id.
+
+    The caller must supply a user authentication token in the
+    query parameters.
+  """
   token_raw = request.args.get('token')
   if not token_raw:
     raise AuthError(AuthStatus.NO_USER_TOKEN_SUPPLIED)
